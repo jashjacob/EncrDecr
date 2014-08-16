24eb255078d467626a7f9d0ef8f574116534deba
Jash Jacob <jash1993@gmail.com>
2014-08-16 13:00:21 +0530

    Indent

--- EncrDecr.java
+++ EncrDecr.java
@@ -4,29 +4,29 @@ import java.util.*;
 
 class EncrDecr
 { 
-	static char[] encrp(String xyz)
+    static char[] encrp(String xyz)
 	{
-       xyz=xyz.toUpperCase(); //  Converting string to uppercase
-       int len=xyz.length();  //length of string
-       final int mid = (xyz.length()+1) / 2;
+        xyz=xyz.toUpperCase(); //  Converting string to uppercase
+        int len=xyz.length();  //length of string
+        final int mid = (xyz.length()+1) / 2;
         
         //Swap 1
         String[] spstr = {xyz.substring(0, mid),xyz.substring(mid)};  //splitting the string into half
         String firstMix=spstr[1]+spstr[0];  //swapping the first half of string with the last half
 
-	    char[] chars=firstMix.toCharArray(); // converting the string into an array for swapping characters
+	char[] chars=firstMix.toCharArray(); // converting the string into an array for swapping characters
 
-	     // Swap 2
-	    swap( chars, 0, chars.length - 2 );
-	    swap( chars, 1, chars.length - 1 );
+	// Swap 2
+	swap( chars, 0, chars.length - 2 );
+	swap( chars, 1, chars.length - 1 );
 
-	     // Swap 3
-	    swap( chars, mid - 1, mid + 1 );
-	    swap( chars, mid - 2, mid );
+	// Swap 3
+	swap( chars, mid - 1, mid + 1 );
+	swap( chars, mid - 2, mid );
 
       //running a character susbtituion 
     
-       for (int i = 0;i < len; i++){
+        for (int i = 0;i < len; i++){
        	if(chars[i]=='A')
        	{
        		chars[i]='@';
@@ -76,7 +76,7 @@ class EncrDecr
 	return chars;	//returning the encrypted string
 }
 
-public static void swap( char[] chars, int l, int r ) {
+    public static void swap( char[] chars, int l, int r ) {
 	char tmp = chars[ l ];
 	chars[ l ] = chars[ r ];
 	chars[ r ] = tmp;
@@ -87,5 +87,5 @@ public static void swap( char[] chars, int l, int r ) {
  	 System.out.println("The string before encrpytion - abcdefghijklmnopqurstuvwxyz");	
 	 System.out.println(encrp("abcdefghijklmnopqurstuvwxyz")); //input string for encryption
 
-	}
+    }
 }
