9efb8f90d7addeae160b655fe50ef5d9c78de9ed
jashjacob <jash1993@gmail.com>
2014-09-17 21:04:36 +0530

    Update Encryption.java

--- Encryption.java
+++ Encryption.java
@@ -2,6 +2,102 @@ import java.lang.*;
 import java.io.*;
 import java.util.*;
 
+class Decryption {
+  static char[] decryptWord(String inputWord)
+  {
+
+  //inputWord=inputWord.toLowerCase(); //  Converting string to uppercase
+  int len=inputWord.length();  //length of string
+  int mid = (inputWord.length()+1) / 2;
+  String s1=new String();
+  String s2=new String();
+  String firstswap=new String();
+  char[] chars=inputWord.toCharArray();
+  //Swap 1
+ 
+    
+        for (int i = 0;i < len; i++){
+        if(chars[i]=='@')
+        {
+          chars[i]='A';
+        }
+        if(chars[i]=='=')
+        {
+          chars[i]='E';
+        }
+        if(chars[i]=='!')
+        {
+          chars[i]='I';
+        }
+        if(chars[i]=='?')
+        {
+          chars[i]='J';
+        }
+        if(chars[i]=='*')
+        {
+          chars[i]='O';
+        }
+        if(chars[i]=='#')
+        {
+          chars[i]='P';
+        }
+        if(chars[i]=='&')
+        {
+          chars[i]='R';
+        }
+        if(chars[i]=='$')
+        {
+          chars[i]='S';
+        }
+        if(chars[i]=='+')
+        {
+          chars[i]='T';
+        }
+        if(chars[i]=='^')
+        {
+          chars[i]='V';
+        }
+        if(chars[i]=='%')
+        {
+          chars[i]='X';
+        }
+       }
+  //swap 3   
+  swap( chars, mid - 1, mid + 1 );
+  swap( chars, mid - 2, mid );
+  
+  //swap 2
+  swap( chars, 0, chars.length - 2 );
+  swap( chars, 1, chars.length - 1 );
+    
+  String s=new String(chars);
+ 
+  if(len%2!=0)
+  {
+    s1=s.substring(0,mid-1);
+    s2=s.substring(mid-1);
+    firstswap=s2.concat(s1);
+    
+  }
+  else
+  {
+  s1=s.substring(0,mid);
+  s2=s.substring(mid);
+  firstswap=s2.concat(s1);
+  }
+  firstswap=firstswap.toLowerCase();
+  
+
+  return firstswap.toCharArray(); //returning the encrypted string
+}
+ public static void swap( char[] chars, int l, int r ) {
+  char tmp = chars[ l ];
+  chars[ l ] = chars[ r ];
+  chars[ r ] = tmp;
+    } 
+}
+
+
 class Encryption
 { 
     static char[] encryptWord(String inputWord)
@@ -85,8 +181,17 @@ class Encryption
 
     public static void main(String[] args)
     {
- 	    System.out.println("The string before encrpytion - abcdefghijklmnopqurstuvwxyz");	
+ 	   Decryption d1= new Decryption();
+      Scanner s=new Scanner(System.in);
+      String temp=s.nextLine();
+      System.out.println(temp);
+ 	    System.out.println("The string before encrpytion - "+temp);	
 
-	    System.out.println(encryptWord("abcdefghijklmnopqurstuvwxyz")); //input string for encryption
+	    System.out.println(encryptWord(temp));
+      char[] s1=encryptWord(temp);
+
+      System.out.println("The string before Decryption is "+ s1);
+	  String s2=new String(s1);
+      System.out.println(d1.decryptWord(s2)); //input string for encryption
     }
 }
